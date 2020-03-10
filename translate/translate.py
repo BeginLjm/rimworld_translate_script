07b0b29b63715ee1cd23195e4617696bfb2c04ee
lujunming <lujunming@bytedance.com>
2020-03-10 10:29:01 +0800

    移除bing翻译

--- translate/translate.py
+++ translate/translate.py
@@ -18,19 +18,21 @@ class Translate:
             print("尝试使用Google翻译:" + source_str + "...")
             target_str = google_translate.google_translate(source_str)
         elif self.translate_vendor == 1:
-            print("尝试使用Bing翻译:" + source_str + "...")
-            target_str = bing_translate.bing_translate(source_str)
-        elif self.translate_vendor == 2:
             print("尝试使用百度翻译:" + source_str + "...")
             target_str = baidu_translate.baidu_translate(source_str)
+        # elif self.translate_vendor == 1:
+        #     print("尝试使用Bing翻译:" + source_str + "...")
+        #     target_str = bing_translate.bing_translate(source_str)
         else:
             print("尝试使用金山翻译:" + source_str + "...")
             target_str = iciba_translate.iciba_translate(source_str)
         if target_str is None:
             self.translate_vendor = self.translate_vendor + 1
             self.try_count = self.try_count + 1
-            if self.try_count >= 4:
-                print("尝试了四个API.居然还是翻译失败了。")
+            # if self.try_count >= 4:
+            if self.try_count >= 3:
+                # print("尝试了四个API.居然还是翻译失败了。")
+                print("尝试了三个API.居然还是翻译失败了。")
                 return None
             return self.translate(source_str)
         else:
